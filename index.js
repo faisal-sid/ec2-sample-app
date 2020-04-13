5fe3740c57d3db16094a1ee1ac3f4a91ce068f83
Gautham <gautham@jnaapti.com>
2020-04-13 18:04:16 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require('express')
+const mysql = require('mysql')
+const app = express()
+
+function getConnection() {
+  return mysql.createConnection({
+    host: process.env.DBHOST,
+    user: process.env.DBUSER,
+    password: process.env.DBPASSWORD,
+    database: 'peopledb'
+  })
+}
+
+function getPeopleCount() {
+  return new Promise((resolve, reject) => {
+    const connection = getConnection()
+    connection.connect()
+    connection.query('SELECT count(*) as count FROM people', function (error, results, fields) {
+      if (error) reject(error)
+      connection.end()
+      resolve(results[0].count)
+    })
+  })
+}
+
+function insertSampleData() {
+  const connection = getConnection()
+  connection.connect()
+  connection.query(`INSERT INTO people(name, email) VALUES
+    ("John", "john@example.com"),
+    ("George", "george@example.com"),
+    ("Jane", "jane@example.org")`, function (error, results, fields) {
+    if (error) throw error
+    connection.end()
+  })
+}
+
+function getPeople() {
+  return new Promise((resolve, reject) => {
+    const connection = getConnection()
+    connection.connect()
+    connection.query('SELECT id, name, email FROM people', function (error, results, fields) {
+      if (error) reject(error)
+      connection.end()
+      resolve(results)
+    })
+  })
+}
+
+function getPerson(id) {
+  return new Promise((resolve, reject) => {
+    const connection = getConnection()
+    connection.connect()
+    connection.query('SELECT id, name, email FROM people WHERE id = ?', [id], function (error, results, fields) {
+      if (error) reject(error)
+      connection.end()
+      if (results.length > 0)
+        resolve(results[0])
+      else
+        resolve(null)
+    })
+  })
+}
+
+async function initialize() {
+  try {
+    const count = await getPeopleCount()
+    if (count === 0)
+      insertSampleData()
+  } catch (e) {
+    console.log(e)
+  }
+
+}
+
+initialize()
+
+app.get('/people', (req, res) => {
+  getPeople().then(data => res.send(JSON.stringify(data)))
+
+})
+
+app.get('/person/:id', (req, res) => {
+  getPerson(parseInt(req.params.id)).then(data => res.send(JSON.stringify(data)))
+})
+
+
+
+app.listen(80, '0.0.0.0', () => console.log('App listening at http://0.0.0.0:80'))
---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Install Node.js
+cd /tmp
+wget https://nodejs.org/dist/v12.16.2/node-v12.16.2-linux-x64.tar.xz
+tar -xJvf node-v12.16.2-linux-x64.tar.xz
+rm node-v12.16.2-linux-x64.tar.xz
+mkdir -p /opt/
+mv node-v12.16.2-linux-x64 /opt/node
+
+# Install the application in /opt
+cd /opt
+git clone https://github.com/buzypi/ec2-sample-app
+cd ec2-sample-app
+export PATH=$PATH:/opt/node/bin
+npm install
+
+# Create the service script
+cat > /etc/systemd/system/app.service <<DELIM
+[Unit]
+Description=EC2 Sample App
+
+[Service]
+ExecStart=/opt/ec2-sample-app/start
+User=root
+
+[Install]
+WantedBy=multi-user.target
+DELIM
+
+systemctl daemon-reload
+systemctl start app
